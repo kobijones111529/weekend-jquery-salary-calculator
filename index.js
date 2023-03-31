b1700d4d3854dc70cdf40ca57ee1d696b0f90cb9
Kobi Jones <kobijonesmail@gmail.com>
2023-03-31 00:39:42 -0500

    Add input form and unstyled data table

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Hello');
+$(document).ready(main);
+
+function main () {
+  $('#form').submit(formSubmit);
+}
+
+function formSubmit (e) {
+  const form = $(e.target);
+  const employee = {
+    firstName: form.children('#form-first-name').val(),
+    lastName: form.children('#form-last-name').val(),
+    id: form.children('#form-id').val(),
+    title: form.children('#form-title').val(),
+    annualSalary: form.children('#form-annual-salary').val()
+  };
+
+  if (employee.firstName.length < 1) {
+    console.log('Employee must have a name');
+    return;
+  }
+
+  addEntry(employee);
+  e.target.reset();
+  form.children('*[autofocus]').focus();
+}
+
+function addEntry (employee) {
+  const makeCell = data => $(`<td>${data}</td>`);
+  const table = $('<tr></tr>')
+    .append(makeCell(employee.firstName))
+    .append(makeCell(employee.lastName))
+    .append(makeCell(employee.id))
+    .append(makeCell(employee.title))
+    .append(makeCell(employee.annualSalary));
+  $('#data').append(table);
+  console.group(`${employee.firstName} ${employee.lastName}`);
+  console.log('ID:', employee.id);
+  console.log('Title:', employee.title);
+  console.log('Salary:', employee.annualSalary);
+  console.groupEnd();
+}
