3c21d5ea2f2a07f8b49830d634e6b7b39dbb3578
Kobi Jones <kobijonesmail@gmail.com>
2023-03-30 21:50:10 -0500

    Basic project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    semi: ['error', 'always']
+  }
+};
